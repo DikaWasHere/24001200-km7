c95dbf6df231cc6f81b4fff0a9c07fd457deb92c
Andika Putra Wijaya <andika.pw2634@gmail.com>
2024-11-10 16:27:04 +0700

    tambahin

--- challenge6/app.js
+++ challenge6/app.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const express = require('express');
-const router = require('./routes/imageRouter');
+require("dotenv").config();
+
+const express = require("express");
+const router = require("./routes/imageRouter");
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
 
-
-app.use('/', router);
-
+app.use("/", router);
 
 app.listen(port, (req, res) => {
-    console.log(`Port ${port} sedang berjalan`);
+  console.log(`Port ${port} sedang berjalan`);
 });
